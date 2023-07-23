fec287449450a98d33b180e58fa956984cfe1bcc
FFengLi <128154514+FFengLi@users.noreply.github.com>
2023-07-23 21:34:52 +0800

    Update insuranceController.java

--- insuranceteam/src/main/java/cn/edu/guet/insuranceteam/controller/insuranceController.java
+++ insuranceteam/src/main/java/cn/edu/guet/insuranceteam/controller/insuranceController.java
@@ -11,26 +11,6 @@ public class insuranceController {
     @Autowired
     private InsuranceListService insuranceListService;
 
-    @GetMapping("/getList")
-    public ResponseData getList(int id) {
-        InsuranceList insuranceList = insuranceListService.getById(id);
-        return ResponseData.ok(insuranceList);
-    }
-
-    @GetMapping("/getList1")
-    public ResponseData getList1(int id) {
-        InsuranceList insuranceList = insuranceListService.getById(id);
-        return ResponseData.ok(insuranceList);
-    }
-
-
-    @GetMapping("insuranceSummary")
-    public ResponseData insuranceSummary(int id) {
-
-        return null;
-    }
-
-
     @PostMapping("/createInsurance")
     public ResponseData createInsurance(@RequestBody InsuranceList insuranceList){
         return insuranceListService.createInsurance(insuranceList);
