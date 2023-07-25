5e26f108fe8fdd6a3335e4436d9b42a54dbeccbd
张gm <826573950@qq.com>
2023-07-25 21:18:19 +0800

    前端主界面

--- insurancevue/src/router/index.js
+++ insurancevue/src/router/index.js
@@ -1,23 +1,21 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import Main from "@/components/Index.vue";
+import Header from "@/components/index/Header.vue";
 
 Vue.use(VueRouter)
 
 const routes = [
-  {
-    path: '/',
-    name: 'home',
-    component: HomeView
-  },
-  {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  }
+    {
+        path: '/main',
+        name: 'main',
+        component: Main
+    },
+    {
+        path: '/head',
+        name:'head',
+        component: Header
+    }
 ]
 
 const router = new VueRouter({
