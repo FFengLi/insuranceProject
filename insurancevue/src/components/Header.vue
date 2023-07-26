fb57bb7249fb5026d8ddcfe7043969705aaeb024
张gm <826573950@qq.com>
2023-07-26 21:20:40 +0800

    Changes

---
+++ insurancevue/src/components/Header.vue
@@ -0,0 +1,26 @@
+<template>
+    <div style="display:flex">
+        <div>
+            <img src="../images/img.png" alt="#" style="width: 45px;margin-top: 5px;margin-left: -30px">
+            <img src="../images/logo.png" alt="#" style="width: 45px;margin-top: 5px" >
+        </div>
+        <div class="text" >
+            <p><span>中国移动传输运维工作台</span></p>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Header"
+}
+</script>
+
+<style scoped>
+.text{
+    font-family: "Microsoft YaHei",sans-serif;
+    font-weight:900;
+    font-size: 18px;
+    color: #409EFF;
+}
+</style>
---
+++ insurancevue/src/views/Transfer.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+        <el-container direction="vertical">
+            <el-header><Header></Header></el-header>
+        </el-container>
+    </div>
+
+</template>
+
+
+<script>
+
+import {defineComponent} from "vue";
+import Header from "@/components/Header.vue";
+
+export default defineComponent({
+    name: "Transfer",
+    components: {
+        Header
+    }
+})
+</script>
+
+
+<style scoped>
+.el-header{
+    /*    background-color: #B3C0D1;*/
+    color: #333;
+    height: 200px;
+}
+</style>
